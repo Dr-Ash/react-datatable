74365acf8f6f117e38f21ae9df83cbb7c44e43a6
Achraf <achraf@procurion.eu>
2021-04-13 12:48:58 +0000

    with material

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from '../style';
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 export default function TableHeader(props){
